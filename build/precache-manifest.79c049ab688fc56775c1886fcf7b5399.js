29c7c03eeb9919b3a97269375283164ce781a94e
Rahul kumar <rahulkumar151995@gmail.com>
2020-05-17 23:48:16 +0530

    changes of build

---
+++ build/precache-manifest.79c049ab688fc56775c1886fcf7b5399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36389e30368865fdfde3384549ac9513",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60059ee8359c8c963355",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b612d2546629e22a6673",
+    "url": "/static/js/2.89c3caf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89c3caf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60059ee8359c8c963355",
+    "url": "/static/js/main.440d45b6.chunk.js"
+  },
+  {
+    "revision": "a24f7caaf99f5302840f",
+    "url": "/static/js/runtime-main.f52d97b4.js"
+  }
+]);
