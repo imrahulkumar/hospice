647e0b5e49b2d3a1a4be7829c59c05a45d70d9c8
Rahul Kumar <rahulkumar151995@gmail.com>
2020-05-22 11:56:30 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd build/
-echo -ne '\n' | surge --domain hospice.surge.sh --token ba8217041b968a724ce4672ec32dfbf6
+echo -ne '\n' | surge --domain hospice.surge.sh --token 'your token'
 echo -ne 'published\n'
